be7a6eadd785455faf649cc951932d851cd6c79b
Yifan Xiao <xiaoyifan@uchicago.edu>
2015-05-04 07:10:21 -0500

    Initial Commit

---
+++ musicStoreFront/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  musicStoreFront
+//
+//  Created by Yifan Xiao on 5/4/15.
+//  Copyright (c) 2015 Yifan Xiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
